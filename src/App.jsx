b4aaf21b75889e0f631a20aa02f72252a1ae7857
Miya096jp <miyawaki.yske@gmail.com>
2025-04-07 11:09:51 +0900

    200番台のステータス以外はerrorとなるようにfetcher内の条件を変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import useSWR from 'swr'
+import useSWR from "swr";
 import "./App.css";
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
+const fetcher = async (...args) => {
+  const res = await fetch(...args);
+
+  if (!res.ok) {
+    const error = new Error("Failed to load");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+  return res.json();
+};
 
 function App() {
-const url = "https://httpstat.us/200?sleep=2000";
-const headers = { Accept: "application/json" };
-const { data, error, isLoading } = useSWR([url, { headers }], ([url, headers]) => fetcher(url, headers))
+  const url = "https://httpstat.us/404";
+  const headers = { Accept: "application/json" };
+  const { data, error, isLoading } = useSWR(
+    [url, { headers }],
+    ([url, headers]) => fetcher(url, headers)
+  );
 
-if (error) return <p>Failed to load</p>
-if (isLoading) return <p>Loading...</p>
+  if (error) return <p>{error.message}</p>;
+  if (isLoading) return <p>Loading...</p>;
 
-return <p>Status : {data.description}</p>
+  return <p>Status : {data.description}</p>;
 }
 
 export default App;
